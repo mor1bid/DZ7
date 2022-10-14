b7ce363fd1997d05c079fc0c5c9a69e26add3dd0
De <spark49942@gmail.com>
2022-10-14 21:23:28 +0300

    SMN

--- smn.py
+++ smn.py
@@ -1,18 +1,33 @@
-# i = 0
-# while i == 0:
-ask = input('Введите желаемый запрос (без пробелов): \n')
-answ = 0
-for i in range(len(ask)):
-    if ask[i].isdigit() or ask[i] == "+" or ask[i] == "*" or ask[i] == "/":
-        if ask[i+1].isdigit():
-            answ += int(ask[i+1])
+prog = 0
+idig = 0
+while prog == 0:
+    ask = input('Введите желаемый запрос (без пробелов): \n')
+    answ = 0
+    for i in range(len(ask)):
+        if i == 0:
+            idig = i+2
+            dig = ask[i]
+        # elif ask[i].isdigit() or ask[i] == "+" or ask[i] == "*" or ask[i] == "/":
         elif ask[i].isdigit():
-            answ += int(ask[i+1])
+            dig += ask[i]
+            idig += 1
+        elif ask[i] == "+":
+            answ += int(dig)
+            dig = ""
+        # elif ask[i].isdigit():
+        #     answ += int(ask[i])
         elif ask[i] == "*" and ask[i+1].isdigit():
-            answ *= int(ask[i+1])
+            answ *= int(dig)
+            dig = ""
         elif ask[i] == "/" and ask[i+1].isdigit():
-            answ /= int(ask[i+1])
-    elif ask[i] == '-' and ask[i+2].isdigit():
-        if ask[i+1].isdigit():
-            answ -= int(ask[i+1])
-print('Ответ:', answ)
+            answ /= int(dig)
+            dig = ""
+        elif ask[i] == '-' and ask[i+1].isdigit():
+            answ -= int(dig)
+            dig = ""
+        i += 1
+        idig += 1
+    print('Ответ:', answ)
+    q = (input('Новый запрос? y/n: '))
+    if q == 'n':
+        prog += 1
