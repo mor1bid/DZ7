a3b4d0ee15dd8c86f694715130c64a4724a666d3
De <spark49942@gmail.com>
2022-10-17 04:24:07 +0300

    DZ-Form1

--- smn.py
+++ smn.py
@@ -1,13 +1,24 @@
 i = 0
-with open('DZFORM1.txt', 'a') as form1:
-    print(*form1)
-    answ = input('\nДобавить новые данные? y/n: ')
-    if answ == 'y':
-        while i == 0:
-            text = input('Введите Фамилию, Имя, Телефон и Описание:')
-            form1.writelines('\ntext')
-            answ = input('Ещё? y/n: ')
-            if answ == 'n':
-                i+=1
-            else:
+with open('DZFORM1.txt', 'a+') as form1:
+    while i == 0:
+        print(*form1)
+        answ = input('\nДобавить новые данные? y/n: ')
+        if answ == 'y':
+            word = 4
+            print('Введите Фамилию, Имя, Телефон и Описание:')
+            while word > 0:
+                text = input()
+                form1.writelines(text)
                 form1.writelines('\n')
+                word -= 1
+        elif answ == 'n':
+            i += 1
+        elif answ != 'n':
+            print('Ошибка!')
+        # answ = input('Ещё? y/n: ')
+        # if answ == 'n':
+        #     i += 1
+        # elif answ == 'y':
+        #     form1.writelines('\n')
+        # else:
+        #     print('Ошибка!')
