ce13c5b3b8fc35f66790bada89c91e16720c7d35
De <spark49942@gmail.com>
2022-10-18 02:20:05 +0300

    DZ-f

--- smn.py
+++ smn.py
@@ -18,32 +18,36 @@ while prog == 0:
             for line in form1:
                 print(line.rstrip('\n'))
     elif work == 'b':
-        form = input('Выберите желаемый формат (список/строка) 1/2:')
+        form = input('Выберите желаемый формат (список/строка) 1/2: ')
         with open('DZFORM1.txt', 'a+') as form1:
             if form == '1':
                 # enu += 1
                 word = 4
-                print('Введите фамилию, имя, телефон и описание (через Enter):')
+                print('Введите фамилию, имя, телефон и описание:')
                 # form1.writelines(str(enu))
                 # form1.writelines('. ')
-                # form1.writelines('\n')
-                while word > 0:
-                    text = input()
-                    form1.writelines(text)
-                    form1.writelines('\n')
-                    word -= 1
-            elif form == '2':
-                print('Введите фамилию, имя, телефон и описание (через Spacebar):')
-                text = input().split(',')
-                form1.writelines(text)
                 form1.writelines('\n')
+                text = input().split(' ')
+                for i in text:
+                    form1.writelines(i)
+                    if i.isdigit() == False:
+                        form1.writelines('\n')
+            elif form == '2':
+                print('Введите фамилию, имя, телефон и описание:')
+                text = input().split(' ')
+                for i in text:
+                    form1.writelines(i)
+                    if i.isdigit() == False or i != len(text):
+                        form1.writelines(',')
     elif work == 'c':
         path = input('Введите адрес желаемого файла и/или его название: \n')
         with open('DZFORM1.txt', 'a+') as form1:
             with open(path, 'r') as impex:
+                form1.writelines('\n')
                 text = impex.readlines()
                 form1.writelines(text)
                 form1.writelines('\n')
+                print('\nГотово!')
     elif work == 'd':
         i = 0
         path = input('Введите адрес желаемого файла и/или его название: \n')
@@ -64,10 +68,11 @@ while prog == 0:
                         i += 1
                     else:
                         print('Ошибка!')
+
     elif work == 'f':
         i = 0
         while i == 0:
-            answ = input('Точно хотите удалить свои данные? y/n: ')
+            answ = input('\nТочно хотите удалить свои данные? y/n: ')
             if answ == 'y':
                 path = input('Введите адрес желаемого файла и/или его название (если файл лежит в директории программы): \n')
                 choi = input('Удалить все данные либо удалить выборочно? 1/2: ')
@@ -76,18 +81,16 @@ while prog == 0:
                         delex.seek(0)
                         delex.truncate()
                         i += 1
-                        # for line in delex:
-                        #     line.rstrip('\n')
                         print('\nГотово!')
                 elif choi == '2':
                     cont = input('Введите фамилию желаемого контакта: \n')
                     with open(path, 'r') as delread:
                         with open(path, 'w') as delex:
                             for line in delread:
-                                if not line.strip('\n').startswith(cont):
+                                if line != cont:
                                     delex.write(line)
+                            i += 1
                             print('\nГотово!')
-                    
                 else:
                     print('Ошибка!')
             elif answ == 'n':
