6ad76ea7cd75642dcda92d4e12e30309b92a4ba7
De <spark49942@gmail.com>
2022-10-17 05:29:25 +0300

    DZ-Form1-a

--- smn.py
+++ smn.py
@@ -1,24 +1,41 @@
-i = 0
-with open('DZFORM1.txt', 'a+') as form1:
-    while i == 0:
-        for line in form1:
-            print(line.rstrip('\n'))
+print('Здравствуйте!', end=' ')
+prog = 0
+ram = 0
+enu = 0
+while prog == 0:
+    work = input('Хотите просмотреть справочник? y/n: ')
+    if work == 'y':
+        with open('DZFORM1.txt', 'r+') as form1:
+            ram = 0
+            print('Ваш справочник: \n')
+            for line in form1:
+                print(line.rstrip('\n'))
+    elif work == 'n':
+        prog += 1
+    else:
+        print('Ошибка!')
+    while ram == 0:
         answ = input('\nДобавить новые данные? y/n: ')
-        if answ == 'y':
-            word = 4
-            print('Введите Фамилию, Имя, Телефон и Описание:')
-            while word > 0:
-                text = input()
-                form1.writelines(text)
+        with open('DZFORM1.txt', 'r+') as form1:
+            if answ == 'y':
+                enu += 1
+                word = 4
+                print('Введите фамилию, имя, телефон и описание (отдельно):')
+                form1.writelines(str(enu))
+                form1.writelines('. ')
                 form1.writelines('\n')
-                word -= 1
-        elif answ == 'n':
-            i += 1
-        elif answ != 'n':
-            print('Ошибка!')
+                while word > 0:
+                    text = input()
+                    form1.writelines(text)
+                    form1.writelines('\n')
+                    word -= 1
+            elif answ == 'n':
+                ram += 1
+            elif answ != 'n':
+                print('Ошибка!')
         # answ = input('Ещё? y/n: ')
         # if answ == 'n':
-        #     i += 1
+        #     ram += 1
         # elif answ == 'y':
         #     form1.writelines('\n')
         # else:
